9ef7145a1ea62d6566248ba8e49cdd2d7e9c0559
Yosuke Ishikawa <y@ishkawa.org>
2016-09-24 16:04:41 +0900

    ls2xs 0.1.0

--- ls2xs.rb
+++ ls2xs.rb
@@ -1,10 +1,11 @@
 class Ls2xs < Formula
-  homepage "https://github.com/ishkawa/LocalizableStrings2XibStrings"
-  url "https://github.com/ishkawa/ls2xs/archive/0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "befa7996c4a69301b7845e21413da3a03dd9dc1c56479f554b562b1cdad91982"
+  homepage "https://github.com/ishkawa/ls2xs"
+  url "https://github.com/ishkawa/ls2xs/archive/0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "cb890dee308e929349b1bac80480ef0f42550d254164757df115786f1cd7ebdb"
 
   def install
     system "make prefix_install PREFIX=#{prefix}"
   end
 end
+
