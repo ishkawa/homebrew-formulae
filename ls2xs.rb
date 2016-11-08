543def7878209bd065ca898cc10ec4f494a9fed7
Yosuke Ishikawa <y@ishkawa.org>
2016-11-09 00:05:02 +0900

    ls2xs 0.2.0

--- ls2xs.rb
+++ ls2xs.rb
@@ -1,8 +1,8 @@
 class Ls2xs < Formula
   homepage "https://github.com/ishkawa/ls2xs"
-  url "https://github.com/ishkawa/ls2xs/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "cb890dee308e929349b1bac80480ef0f42550d254164757df115786f1cd7ebdb"
+  url "https://github.com/ishkawa/ls2xs/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "df6de24f6ef3d85df3f8a07761d861dd6221090559c97d821079d130e1b0688d"
 
   def install
     system "make prefix_install PREFIX=#{prefix}"
